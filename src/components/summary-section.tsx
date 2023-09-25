0877a4f6dcd2a7937d0ffd6764f7aa36fe1ac1be
pouriversal <pooriversal@gmail.com>
2023-09-25 23:54:49 +0330

    fix: change some content

--- src/components/summary-section.tsx
+++ src/components/summary-section.tsx
@@ -5,17 +5,17 @@ import { FileText, Mail, MapPin, Smartphone } from "lucide-react";
 export default function Summary({ dictionaries }: { dictionaries: any }) {
   return (
     <section>
-      <Accordion
-        title={dictionaries.summary.title}
-        icon={<FileText className="dark:text-white text-gray-800" />}
-      >
+      <Accordion showBackground title={dictionaries.summary.title} icon={<FileText className="dark:text-white text-gray-800" />}>
         <p className="dark:text-white text-gray-800 font-normal opacity-90 text-justify mb-5">
           {dictionaries.summary.content.about}
         </p>
         <div className="flex flex-col justify-between lg:items-center gap-3 items-start w-full lg:w-4/5 lg:flex-row ltr">
           <div className="flex dark:text-white text-gray-800 gap-2 items-center ltr">
             <Mail className="dark:text-sky-100 text-gray-500" />
-            <a href="mailto:pooriversal@gmail.com" className="dark:text-sky-200 text-sky-500 underline font-medium dark:font-normal">
+            <a
+              href="mailto:pooriversal@gmail.com"
+              className="dark:text-sky-200 text-sky-500 underline font-medium dark:font-normal"
+            >
               {dictionaries.summary.content.email}
             </a>
           </div>
@@ -27,7 +27,10 @@ export default function Summary({ dictionaries }: { dictionaries: any }) {
           </div>
           <div className="flex dark:text-white text-gray-800 gap-2 items-center ltr">
             <MapPin className="dark:text-sky-100 text-gray-500" />
-            <a href="https://goo.gl/maps/BodBqXXqN3yaKkoH8" className="dark:text-sky-200 text-sky-500 underline font-medium dark:font-normal">
+            <a
+              href="https://goo.gl/maps/BodBqXXqN3yaKkoH8"
+              className="dark:text-sky-200 text-sky-500 underline font-medium dark:font-normal"
+            >
               {dictionaries.summary.content.location}
             </a>
           </div>
